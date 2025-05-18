6566c42382e45f0d0bc7b5657ac369e3bf46b4e6
Valentín Veintemilla <valentinveintemilla@gmail.com>
2025-05-18 13:09:58 -0300

    Registra los services restantes en AppServiceProvider

--- eRemate/app/Providers/AppServiceProvider.php
+++ eRemate/app/Providers/AppServiceProvider.php
@@ -3,16 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\Chat\ChatServiceInterface;
-use App\Services\Chat\ChatService;
-use App\Services\Mensaje\MensajeServiceInterface;
-use App\Services\Mensaje\MensajeService;
-use App\Services\Factura\FacturaServiceInterface;
-use App\Services\Factura\FacturaService;
-use App\Services\Compra\CompraServiceInterface;
-use App\Services\Compra\CompraService;
+use App\Services\Articulo\ArticuloServiceInterface;
+use App\Services\Articulo\ArticuloService;
+use App\Services\Auth\AuthServiceInterface;
+use App\Services\Auth\AuthService;
 use App\Services\Calificacion\CalificacionServiceInterface;
 use App\Services\Calificacion\CalificacionService;
+use App\Services\CasaDeRemates\CasaDeRematesServiceInterface;
+use App\Services\CasaDeRemates\CasaDeRematesService;
+use App\Services\Chat\ChatServiceInterface;
+use App\Services\Chat\ChatService;
+use App\Services\Compra\CompraServiceInterface;
+use App\Services\Compra\CompraService;
+use App\Services\Factura\FacturaServiceInterface;
+use App\Services\Factura\FacturaService;
+use App\Services\Lote\LoteServiceInterface;
+use App\Services\Lote\LoteService;
+use App\Services\Mensaje\MensajeServiceInterface;
+use App\Services\Mensaje\MensajeService;
+use App\Services\Rematador\RematadorServiceInterface;
+use App\Services\Rematador\RematadorService;
+use App\Services\Subasta\SubastaServiceInterface;
+use App\Services\Subasta\SubastaService;
+use App\Services\Usuario\UsuarioServiceInterface;
+use App\Services\Usuario\UsuarioService;
 use App\Services\UsuarioRegistrado\UsuarioRegistradoServiceInterface;
 use App\Services\UsuarioRegistrado\UsuarioRegistradoService;
 
@@ -23,26 +37,76 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //Servicio para el Chat
-        $this->app->bind(ChatServiceInterface::class, ChatService::class);
-        
-        //Servicio para los Mensajes
-        $this->app->bind(MensajeServiceInterface::class, MensajeService::class);
-
-        //Servicio para Factura
+        //Servicio para Articulo
         $this->app->bind(
-            FacturaServiceInterface::class, FacturaService::class
+            ArticuloServiceInterface::class,
+            ArticuloService::class
         );
 
-        //Servicio para Compra
+        //Servicio para Auth
         $this->app->bind(
-            CompraServiceInterface::class, CompraService::class
+            AuthServiceInterface::class,
+            AuthService::class
         );
 
         //Servicio para Calificacion
         $this->app->bind(
             CalificacionServiceInterface::class,
             CalificacionService::class
+        );
+
+        //Servicio para Casa de Remates
+        $this->app->bind(
+            CasaDeRematesServiceInterface::class,
+            CasaDeRematesService::class
+        );
+
+        //Servicio para el Chat
+        $this->app->bind(
+            ChatServiceInterface::class,
+            ChatService::class
+        );
+        
+        //Servicio para Compra
+        $this->app->bind(
+            CompraServiceInterface::class,
+            CompraService::class
+        );
+
+        //Servicio para Factura
+        $this->app->bind(
+            FacturaServiceInterface::class,
+            FacturaService::class
+        );
+
+        //Servicio para Lote
+        $this->app->bind(
+            LoteServiceInterface::class,
+            LoteService::class
+        );
+        
+        //Servicio para los Mensajes
+        $this->app->bind(
+            MensajeServiceInterface::class,
+            MensajeService::class
+        );
+
+        //Servicio para Rematador
+        $this->app->bind(
+            RematadorServiceInterface::class,
+            RematadorService::class
+        );
+
+        //Servicio para Subasta
+        $this->app->bind(
+            SubastaServiceInterface::class,
+            SubastaService::class
+        );
+
+        //Servicio para Usuario
+        $this->app->bind(
+            UsuarioServiceInterface::class,
+            UsuarioService::class
         );
 
         //Servicio para Usuario Registrado
