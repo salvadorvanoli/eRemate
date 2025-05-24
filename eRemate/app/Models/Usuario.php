4677e8b09a59aac576eb863c9976a152ee39af19
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2025-05-24 13:08:36 -0300

    Solucionado el error al realizar login

--- eRemate/app/Models/Usuario.php
+++ eRemate/app/Models/Usuario.php
@@ -22,19 +22,19 @@ class Usuario extends Model
         'tipo'
     ];
 
+    public function usuarioRegistrado()
+    {
+        return $this->hasOne(UsuarioRegistrado::class, 'id', 'id');
+    }
+
     public function rematador()
     {
-        return $this->hasOne(Rematador::class);
+        return $this->hasOne(Rematador::class, 'id', 'id');
     }
 
     public function casaDeRemates()
     {
-        return $this->hasOne(CasaDeRemates::class);
-    }
-
-    public function usuarioRegistrado()
-    {
-        return $this->hasOne(UsuarioRegistrado::class);
+        return $this->hasOne(CasaDeRemates::class, 'id', 'id');
     }
 
     public function esRematador()
